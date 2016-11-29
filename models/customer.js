4ffe0520d4bc578fed3f16b9216e8fb6fe988458
Alonso Godinez <alonxogs@gmail.com>
2016-11-29 12:09:56 -0500

    add fields to Customer model

--- models/customer.js
+++ models/customer.js
@@ -3,11 +3,14 @@
 module.exports = (sequelize, DataTypes) => {
 
     const Customer = sequelize.define("Customer", {
-        name: DataTypes.STRING(50),
+        first_name: DataTypes.STRING(50),
+        last_name: DataTypes.STRING(50),
         address: DataTypes.STRING(50),
         phone: DataTypes.STRING(10),
         cellphone: DataTypes.STRING(20),
-        email: DataTypes.STRING(50)
+        email: DataTypes.STRING(50),
+        dni: DataTypes.STRING(8)
+
     }, {
         classMethods: {
             associate: (models) => {
