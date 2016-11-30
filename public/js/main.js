b9ffecad952061a59c6e9dc4fa8dcd4f7d0bb090
Alonso Godinez <alonxogs@gmail.com>
2016-11-30 12:24:25 -0500

    adding forms on inputs modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,35 @@
 $(function(){
     "use strict";
 
-    alert('test')
+    var  quotationModal = $('#quotationInputModal');
+
+    quotationModal.modal('show');
+
+    $('#inputType').change(function(){
+        var inputForm = $('.inputForm');
+        inputForm.addClass('hidden');
+
+        if(this.value == 'papel'){
+            $('#paperForm').removeClass('hidden');
+        }
+        else if(this.value == 'placas'){
+            $('#plateForm').removeClass('hidden');
+        }
+        else if(this.value == 'impresion'){
+            $('#printForm').removeClass('hidden');
+        }
+        else if(this.value == 'acabado'){
+            $('#finishForm').removeClass('hidden');
+        }
+        else if(this.value == 'fotolitos'){
+            $('#photolithForm').removeClass('hidden');
+        }
+        else if(this.value == 'troquelados'){
+            $('#punchingForm').removeClass('hidden');
+        }
+        else if(this.value == 'plastificado'){
+            $('#plasticizingForm').removeClass('hidden');
+        }
+    })
 
 });
