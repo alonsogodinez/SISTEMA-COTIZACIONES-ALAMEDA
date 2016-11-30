6937c5ec20e853656c0138ea8cf71ce664d51b4c
Alonso Godinez <alonxogs@gmail.com>
2016-11-30 11:24:47 -0500

    adding bstrp,jquery, modal

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/cotizaciones', (req, res) => {
+  res.render('quotation')
+});
+
 module.exports = router;
 
 
